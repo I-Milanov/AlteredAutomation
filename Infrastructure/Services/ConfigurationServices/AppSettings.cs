2f2a77696e8a073dc68d7201ece4a73764ba81bc
IvayloMilanov <i.t.milanov1996@gmail.com>
2025-02-23 21:50:14 +0200

    Create configuration service

---
+++ Infrastructure/Services/ConfigurationServices/AppSettings.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+
+namespace Infrastructure.Services
+{
+    public class TimeoutSettings
+    {
+        public int Main { get; set; }
+        public int UntilTotalTimeout { get; set; }
+        public int ForAjax { get; set; }
+    }
+
+    public class BrowserOptions
+    {
+
+        public bool Headless { get; set; }
+        public bool StartMaximized { get; set; }
+        public bool DisableNotifications { get; set; }
+        public bool AcceptInsecureCertificates { get; set; }
+        public string UnhandledPromptBehavior { get; set; }
+
+        public UnhandledPromptBehavior GetUnhandledPromptBehavior()
+        {
+            return Enum.Parse<UnhandledPromptBehavior>(UnhandledPromptBehavior);
+        }
+    }
+
+    public class AppSettings
+    {
+        public TimeoutSettings Timeout { get; set; }
+        public BrowserOptions BrowserOptions { get; set; }
+    }
+}
